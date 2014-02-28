d71cd5c34a3e24ac76ee62833e18f1798c5cc983
Stephen Kell <srk31@cl.cam.ac.uk>
2014-02-28 15:47:31 +0000

    Minor tweaks to heap index: use 48 not 64 bits for alloc_site on x86-64, and get rid of 'self-contained' .c-file idea.

--- include/heap_index.h
+++ include/heap_index.h
@@ -17,10 +17,15 @@ struct entry
 extern struct entry *index_region __attribute__((weak));
 
 #define WORD_BITSIZE ((sizeof (void*))<<3)
+#if defined(__x86_64__) || defined(x86_64)
+#define ADDR_BITSIZE 48
+#else
+#define ADDR_BITSIZE WORD_BITSIZE
+#endif
 struct trailer
 {
 	unsigned alloc_site_flag:1;
-	unsigned long alloc_site:(WORD_BITSIZE-1);
+	unsigned long alloc_site:(ADDR_BITSIZE-1);
 #ifdef HEAP_INDEX_TRAILER_INCLUDE
 #include HEAP_INDEX_TRAILER_INCLUDE
 #endif
@@ -36,9 +41,13 @@ struct trailer *lookup_object_info(const void *mem, void **out_object_start) __a
  * Platforms without TLS have to do without this feature. */
 #ifndef NO_TLS
 extern __thread void *__current_allocsite;
+extern __thread void *__current_allocfn;
+extern __thread size_t __current_allocsz;
 #else
-#warning "Compiling without __current_allocsite TLS variable."                  
-#define __current_allocsite ((void*)0)                                          
+#warning "Using thread-unsafe __current_allocsite variable."
+extern void *__current_allocsite;
+extern void *__current_allocfn;
+extern size_t __current_allocsz;
 #endif
 
 #endif
